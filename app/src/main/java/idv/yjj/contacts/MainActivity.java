9c1fe72e995e4fb129daaf95bfabf6dee8b06e98
winricyu <winricyu@gmail.com>
2016-08-17 16:23:09 +0800

    - 專案建置

---
+++ app/src/main/java/idv/yjj/contacts/MainActivity.java
@@ -0,0 +1,17 @@
+package idv.yjj.contacts;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
