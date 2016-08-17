0749afaa1105220e798a1f19e7ae3e11ec776e3a
winricyu <winricyu@gmail.com>
2016-08-17 18:16:49 +0800

    - 聯絡人資料清單

--- app/src/main/java/idv/yjj/contacts/MainActivity.java
+++ app/src/main/java/idv/yjj/contacts/MainActivity.java
@@ -1,10 +1,16 @@
 package idv.yjj.contacts;
 
 import android.content.ContentResolver;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ContentResolver cr = this.getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
+        Cursor cursor = cr.query(uri, null, null, null, null);
+
+
+        while (cursor.moveToNext()) {
+            String name =cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+            String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            Log.d("cursor",name+" / "+phoneNumber);
+        }
+
+
+        String[] columns = new String[]{
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.Contacts.Photo.PHOTO
+        };
+        int[] resID = new int[]{
+                R.id.text_name, R.id.text_number, R.id.img_photo
+        };
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.contact_item, cursor, columns, resID, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        ListView listView = (ListView) this.findViewById(R.id.listView);
+        listView.setAdapter(cursorAdapter);
+
+
+        /*cursorAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                return false;
+            }
+        });*/
 
     }
 }
